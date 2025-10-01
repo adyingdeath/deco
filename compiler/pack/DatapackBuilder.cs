f61ae258f8de8a970509f30c9b31c001ad3ce8e0
adyingdeath <2975527435@qq.com>
2025-10-01 14:41:21 +0800

    feat: complete part of return if generation.

--- compiler/pack/DatapackBuilder.cs
+++ compiler/pack/DatapackBuilder.cs
@@ -46,73 +46,88 @@ public class DatapackBuilder(Datapack datapack) : IRVisitor<List<string>> {
         var location = new ResourceLocation(
             _datapack.Namespace, inst.Target.Label
         );
-        return [$"function {location}"];
+        return [$"execute store result score 0 {_datapack.Id} run function {location}"];
     }
 
     public override List<string> VisitLinkInstruction(LinkInstruction inst) {
         return [];
     }
 
-    public override List<string> VisitMoveInstruction(MoveInstruction inst)
-    {
-        var commands = new List<string>();
-
-        switch (inst.Source, inst.Destination)
-        {
+    public override List<string> VisitMoveInstruction(MoveInstruction inst) {
+        return (inst.Source, inst.Destination) switch {
             // Destination cannot be a ConstantOperand in a move operation.
-            case (_, ConstantOperand dest):
-                commands.Add($"# ERROR: Destination cannot be a ConstantOperand: {dest.Value}");
-                break;
+            (_, ConstantOperand dest) =>
+                [$"# ERROR: Destination cannot be a ConstantOperand: {dest.Value}"],
 
             // --- Destination is ScoreboardOperand ---
-            case (ConstantOperand source, ScoreboardOperand dest):
+            (ConstantOperand source, ScoreboardOperand dest) =>
                 // Move constant to scoreboard:
                 // /scoreboard players set <target> <objective> <value>
-                commands.Add($"scoreboard players set {dest.Code} {_datapack.Id} {source.Value}");
-                break;
+                [$"scoreboard players set {dest.Code} {_datapack.Id} {source.Value}"],
 
-            case (ScoreboardOperand source, ScoreboardOperand dest):
+            (ScoreboardOperand source, ScoreboardOperand dest) =>
                 // Move scoreboard to scoreboard:
                 // /scoreboard players operation <destTarget> <destObj> = <sourceTarget> <sourceObj>
-                commands.Add($"scoreboard players operation {dest.Code} {_datapack.Id} = {source.Code} {_datapack.Id}");
-                break;
+                [$"scoreboard players operation {dest.Code} {_datapack.Id} = {source.Code} {_datapack.Id}"],
 
-            case (StorageOperand source, ScoreboardOperand dest):
+            (StorageOperand source, ScoreboardOperand dest) =>
                 // Move storage to scoreboard:
                 // /execute store result score <target> <objective> run data get storage <id> <path>
-                commands.Add($"execute store result score {dest.Code} {_datapack.Id} run data get storage {_datapack.Id} {source.Code}");
-                break;
+                [$"execute store result score {dest.Code} {_datapack.Id} run data get storage {_datapack.Id} {source.Code}"],
 
             // --- Destination is StorageOperand ---
-            case (ConstantOperand source, StorageOperand dest):
+            (ConstantOperand source, StorageOperand dest) =>
                 // Move constant to storage:
                 // /data modify storage <id> <path> set value <value>
-                commands.Add($"data modify storage {_datapack.Id} {dest.Code} set value {source.Value}");
-                break;
+                [$"data modify storage {_datapack.Id} {dest.Code} set value {source.Value}"],
 
-            case (ScoreboardOperand source, StorageOperand dest):
+            (ScoreboardOperand source, StorageOperand dest) =>
                 // Move scoreboard to storage:
                 // /execute store result storage <id> <path> float 1.0 run scoreboard players get <target> <objective>
-                commands.Add($"execute store result storage {_datapack.Id} {dest.Code} float 1.0 run scoreboard players get {source.Code} {_datapack.Id}");
-                break;
+                [$"execute store result storage {_datapack.Id} {dest.Code} float 1.0 run scoreboard players get {source.Code} {_datapack.Id}"],
 
-            case (StorageOperand source, StorageOperand dest):
+            (StorageOperand source, StorageOperand dest) =>
                 // Move storage to storage:
                 // /data modify storage <destId> <destPath> set from storage <sourceId> <sourcePath>
-                commands.Add($"data modify storage {_datapack.Id} {dest.Code} set from storage {_datapack.Id} {source.Code}");
-                break;
+                [$"data modify storage {_datapack.Id} {dest.Code} set from storage {_datapack.Id} {source.Code}"],
 
-            // Fallback for unhandled combinations (should not be reached if all 3x3 cases are covered, except invalid dest constant)
-            default:
-                commands.Add($"# WARNING: Unhandled MoveInstruction combination: Source={inst.Source.GetType().Name}, Destination={inst.Destination.GetType().Name}");
-                break;
-        }
-
-        return commands;
+            _ => []
+        };
     }
 
     public override List<string> VisitReturnIfInstruction(ReturnIfInstruction inst) {
-        return [];
+        return (inst.Condition.Left, inst.Condition.Left) switch {
+            (ConstantOperand left, ConstantOperand right) =>
+                left.Value.Equals(right.Value) ? [$"return {GenOperand(inst.Value)}"] : [],
+
+            // ----- Constant and Scoreboard -----
+            (ScoreboardOperand left, ConstantOperand right) =>
+                // /execute if score <destTarget> <destObj> matches <value>
+                [$"execute if score {left.Code} {_datapack.Id} matches {right.Value} return {GenOperand(inst.Value)}"],
+            (ConstantOperand left, ScoreboardOperand right) =>
+                // /execute if score <destTarget> <destObj> matches <value>
+                [$"execute if score {right.Code} {_datapack.Id} matches {left.Value} return {GenOperand(inst.Value)}"],
+            (ScoreboardOperand left, ScoreboardOperand right) =>
+                // Move scoreboard to scoreboard:
+                // /execute if score <destTarget> <destObj> = <sourceTarget> <sourceObj>
+                [$"execute if score {left.Code} {_datapack.Id} = {right.Code} {_datapack.Id} run return {GenOperand(inst.Value)}"],
+
+
+            (ScoreboardOperand left, StorageOperand right) =>
+                [],
+            (StorageOperand left, ScoreboardOperand right) =>
+                [],
+
+            // --- Destination is StorageOperand ---
+            (StorageOperand left, ConstantOperand right) =>
+                [],
+            (ConstantOperand left, StorageOperand right) =>
+                [],
+            (StorageOperand left, StorageOperand right) =>
+                [],
+
+            _ => []
+        };
     }
 
     public override List<string> VisitReturnInstruction(ReturnInstruction inst) {
@@ -121,5 +136,13 @@ public class DatapackBuilder(Datapack datapack) : IRVisitor<List<string>> {
 
     public override List<string> VisitUnaryInstruction(UnaryInstruction inst) {
         return [];
+    }
+
+    public string GenOperand(Operand? operand) {
+        return operand switch {
+            ConstantOperand constant => constant.Value,
+            VariableOperand variable => variable.Code,
+            _ => ""
+        };
     }
 }
